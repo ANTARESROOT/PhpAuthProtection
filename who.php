7042ccc9b8170ff1427acb62f255dea2989a58a6
ANTARESROOT <48474689+ANTARESROOT@users.noreply.github.com>
2019-06-04 14:28:21 +0300

    Add files via upload

--- who.php
+++ who.php
@@ -1,7 +1,15 @@
 <?php
-$addr = $_SERVER["REMOTE_ADDR"];
+$client  = @$_SERVER['HTTP_CLIENT_IP'];
+$forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+$remote  = @$_SERVER['REMOTE_ADDR'];
+
+if(filter_var($client, FILTER_VALIDATE_IP)) $ip = $client;
+elseif(filter_var($forward, FILTER_VALIDATE_IP)) $ip = $forward;
+else $ip = $remote;
+
+//$addr = $_SERVER["REMOTE_ADDR"];
 $d = date(DATE_RFC822);
-$string = $addr . " --- " . $d . "\n";
+$string = $ip . " --- " . $d . "\n";
 file_put_contents("ip.txt", $string, FILE_APPEND);
 
 ?>
